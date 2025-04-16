f654474d8251f2b390b6b4ef07c0a03707b47ec7
nsalyzyn <nsalyzyn@gmail.com>
2025-04-16 17:04:11 -0600

    Removing getElementById usages

--- app/profile/invoice.tsx
+++ app/profile/invoice.tsx
@@ -1,38 +1,42 @@
 'use client';
 
+import { useRef } from 'react';
+
 export function InvoiceCalculator() {
+    const invoiceRef = useRef(null)
+    const paycycleRef = useRef(null)
+    const paymentRef = useRef(null)
+
     return (
         <>
             <label>Invoice due date</label>
-            <input type="date" name="invoice" id="invoice" onChange={updatePaymentDate} />
+            <input type="date" name="invoice" ref={invoiceRef} onChange={updatePaymentDate} />
             <br/>
             <label>Monthly paycycle date</label>
-            <input type="date" name="paycycle" id="paycycle" onChange={updatePaymentDate} />
+            <input type="date" name="paycycle" ref={paycycleRef} onChange={updatePaymentDate} />
             <br/>
-            Supplier should expect a payment on: <span id="payment">unknown date</span>
+            Supplier should expect a payment on: <span ref={paymentRef}>unknown date</span>
         </>
     )
-}
 
-
-function updatePaymentDate() {
-    // TODO: Don't fetch by ID - that's gross
-    const invoice = document.getElementById('invoice') as HTMLInputElement;
-    const paycycle = document.getElementById('paycycle') as HTMLInputElement;
-    const payment = document.getElementById('payment');
-
-    if (!invoice.value || !paycycle.value) {
-        payment?.setHTMLUnsafe("unknown date")
-        return;
-    }
-    const invoiceDate = new Date(invoice.value + "T12:00:00")
-    const paycycleDate = new Date(paycycle.value + "T12:00:00")
-    const payDate = new Date(invoiceDate.valueOf())
+    function updatePaymentDate() {
+        const invoice = invoiceRef.current as unknown as HTMLInputElement;
+        const paycycle = paycycleRef.current as unknown as HTMLInputElement;
+        const payment = paymentRef.current as unknown as HTMLElement;
     
-    payDate.setDate(paycycleDate.getDate())
-    if (invoiceDate.getDate() > paycycleDate.getDate()) {
-        payDate.setMonth(payDate.getMonth() + 1)
-    }
-
-    payment?.setHTMLUnsafe(payDate.toDateString())
+        if (!invoice.value || !paycycle.value) {
+            payment.setHTMLUnsafe("unknown date")
+            return;
+        }
+        const invoiceDate = new Date(invoice.value + "T12:00:00")
+        const paycycleDate = new Date(paycycle.value + "T12:00:00")
+        const payDate = new Date(invoiceDate.valueOf())
+        
+        payDate.setDate(paycycleDate.getDate())
+        if (invoiceDate.getDate() > paycycleDate.getDate()) {
+            payDate.setMonth(payDate.getMonth() + 1)
+        }
+    
+        payment.setHTMLUnsafe(payDate.toDateString())
+    }    
 }
