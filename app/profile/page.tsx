2eda27801306c0595a645126b51e3c73c654fa60
nsalyzyn <nsalyzyn@gmail.com>
2025-04-16 15:00:24 -0600

    Switch to a redirect based on requirements.

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,17 +1,20 @@
 import styles from "/app/page.module.css";
 import { getUser } from '@/app/actions/auth'
+import { redirect } from 'next/navigation'
 
 export default async function Home() {
   const userData = await getUser()
+  // TODO: Return 401 when supported by NextJS (See https://github.com/vercel/next.js/discussions/49302)
   if (!userData.success) {
-    return (
-      <div className={styles.page}>
-        <h1>401</h1>
-        <div>
-          {userData.message}
-        </div>
-      </div>
-    )
+    redirect('/')
+    // return (
+    //   <div className={styles.page}>
+    //     <h1>401</h1>
+    //     <div>
+    //       {userData.message}
+    //     </div>
+    //   </div>
+    // )
   }
 
   return (
